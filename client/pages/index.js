6bb30f724fdea761e9c3380c7c83fe2db21e8192
Akash-5675 <99992480+Akash-5675@users.noreply.github.com>
2023-02-02 23:06:25 +0530

    navbar

---
+++ client/pages/index.js
@@ -0,0 +1,24 @@
+// navbar js goes here
+
+const navbar = document.querySelector('.navbar');
+
+navbar.querySelector('.toggle').addEventListener('click', () => {
+
+  navbar.classList.toggle('collapsed');
+
+});
+
+
+
+window.addEventListener('scroll', e => {
+
+  let windowY = window.pageYOffset;
+
+  let navbarHeight = document.querySelector('.navbar').offsetHeight;
+
+  if (windowY > navbarHeight) navbar.classList.add('sticky');else
+  navbar.classList.remove('sticky');
+
+});
+
+// navbar js ends here
