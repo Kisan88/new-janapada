3166db98e551ba5a9a9f9aec526cf51a63049332
Akash-5675 <99992480+Akash-5675@users.noreply.github.com>
2023-02-04 17:51:45 +0530

    index navbar

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,14 +3,21 @@
 document.onreadystatechange = function() {
   let lastScrollPosition = 0;
   const navbar = document.querySelector('.navbar');
+  const service = document.querySelector('.service');
   window.addEventListener('scroll', function(e) {
     lastScrollPosition = window.scrollY;
     
-    if (lastScrollPosition > 10)
+    if (lastScrollPosition > 10){
       navbar.classList.add('navbar-dark');
-    else
+      service.classList.add('service-dark');
+    }
+    else{
       navbar.classList.remove('navbar-dark');
+      service.classList.remove('service-dark');
+    }
   });
 }
+
+
 
 // navbar js ends here
