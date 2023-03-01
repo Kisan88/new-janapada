e6e579e670043896d00b9432c35a2bc066c1dcea
Akash-5675 <99992480+Akash-5675@users.noreply.github.com>
2023-03-01 18:02:01 +0530

    navbar to cart page

--- client/pages/cart.js
+++ client/pages/cart.js
@@ -1,20 +1,35 @@
-document.onreadystatechange = function() {
-    let lastScrollPosition = 0;
-    const navbar = document.querySelector('.navbar');
-    const service = document.querySelector('.service');
-    window.addEventListener('scroll', function(e) {
-      lastScrollPosition = window.scrollY;
-      
-      if (lastScrollPosition > 10){
-        navbar.classList.add('navbar-dark');
-        service.classList.add('service-dark');
-      }
-      else{
-        navbar.classList.remove('navbar-dark');
-        service.classList.remove('service-dark');
-      }
-    });
-  }
+// navbar js goes here
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+// sectionOneObserver.observe(sectionOne);
+
+document.querySelector("#check").addEventListener("change", (e) => {
+  (document.querySelector("#check").checked)? document.querySelector("#navb").classList.add("active"): document.querySelector("#navb").classList.remove("active")
+})
+  
+  
+  
+  // navbar js ends here
+
 
   /* Set rates + misc */
 var taxRate = 0.05;
